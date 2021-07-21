6c416e6462ad4c4c295218652d56dab7845102d9
Jay Dampitan <jaykenethpd@gmail.com>
2021-07-21 10:02:53 -0700

    style(navigation-styling): add styling and links to the navigation bar

--- pokeapp/src/components/Navigation.js
+++ pokeapp/src/components/Navigation.js
@@ -3,9 +3,26 @@ import React from "react";
 const Navigation = () => {
 
     return(
-        <div className="navigation">
-            Navigation 
-        </div>
+        
+            <nav className="navigation">
+               <div className="navigation-logo">
+                   <div>Logo</div>
+               </div>
+                <div className="navigation-links">
+                    <div className="card-links">
+                        <h4>Pokedex</h4>
+                        <h4>MyPoke</h4>
+                    </div> 
+                    <div className="coin-link">
+                        <div className="coin-link-total">
+                            <h4>$1,357</h4>
+                        </div>
+                        <div className="coin-link-earn">
+                            <h4>Earn Coins</h4>
+                        </div>
+                    </div>                   
+                </div>
+            </nav>
     )
 };
 
