779c3db9cbd2932905c7741ad3111bf0408471c2
Kenneth Ferrell <kferrell@alphaworks.tech>
2021-07-21 11:19:11 -0700

    feat(styling):style landing page

--- pokeapp/src/components/FilterBar.js
+++ pokeapp/src/components/FilterBar.js
@@ -154,47 +154,9 @@ const FilterBar = ({
 
   return (
     <div className="filter-bar">
-      Filter bar
-
-      <div>
-        <label>Low to High</label>
-        {radioEl(SortTypes.ID_LOW_TO_HIGH)}
-
-        <label>High to Low</label>
-        {radioEl(SortTypes.ID_HIGH_TO_LOW)}
-
-        <label>A-Z</label>
-        {radioEl(SortTypes.A_TO_Z)}
-
-        <label>Z-A</label>
-        {radioEl(SortTypes.Z_TO_A)}
-
-        <label>Value High</label>
-        {radioEl(SortTypes.VALUE_HIGH_TO_LOW)}
-
-        <label>Value Low</label>
-        {radioEl(SortTypes.VALUE_LOW_TO_HIGH)}
-      </div>
-
-      <div className="checkbox-type">
-        {checkboxEl(ElementTypes.FIRE, `Fire`)}
-        {checkboxEl(ElementTypes.WATER, `Water`)}
-        {checkboxEl(ElementTypes.ICE, `Ice`)}
-        {checkboxEl(ElementTypes.GRASS, `Grass`)}
-        {checkboxEl(ElementTypes.GHOST, `Ghost`)}
-        {checkboxEl(ElementTypes.GROUND, `Ground`)}
-        {checkboxEl(ElementTypes.FLYING, `Flying`)}
-        {checkboxEl(ElementTypes.BUG, `Bug`)}
-        {checkboxEl(ElementTypes.DARK, `Dark`)}
-        {checkboxEl(ElementTypes.DRAGON, `Dragon`)}
-        {checkboxEl(ElementTypes.ELECTRIC, `Electric`)}
-        {checkboxEl(ElementTypes.FAIRY, `Fairy`)}
-        {checkboxEl(ElementTypes.POISON, `Poison`)}
-        {checkboxEl(ElementTypes.FIGHTING, `Fighting`)}
-        {checkboxEl(ElementTypes.STEEL, `Steel`)}
-        {checkboxEl(ElementTypes.PSYCHIC, `Psychic`)}
-        {checkboxEl(ElementTypes.NORMAL, `Normal`)}
-        {checkboxEl(ElementTypes.ROCK, `Rock`)}
+      <h4 className="filter-bar-text">Filter Pokemon</h4>
+      <div className="filter-bar-icon">
+        Icon
       </div>
     </div>
   );
